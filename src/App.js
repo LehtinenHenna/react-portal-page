b1e6838af812598c60d1dc61c9205d63d277a8ec
Henna Lehtinen <henna@iontau.com>
2024-06-26 21:51:01 +0300

    adding background, changing title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 //import logo from './logo.svg';
 import './App.css';
+import background from "./186984205_1116806818830150_8166379831712192046_n.jpg";
 
 function App() {
   return (
-    <div className="App">
-        <h1>Portal</h1>
+    <div  style={{
+        backgroundImage: `url(${background})`,
+        height: '800px'
+      }} 
+      className="App">
+        <h1>
+          Avaran uskon foorumi
+        </h1>
         <ul>
           <a href="https://facebook.com">
             <img src="facebook-logo3.png" className="logo" alt="facebook logo"/>
